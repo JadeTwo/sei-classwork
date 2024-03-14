b9e9863a83e3ce13a42bd8602c2af3b0bb524714
jadedrb <jadecdrb@gmail.com>
2024-03-14 12:41:57 -0400

    added some variable examples

--- 308-intro-javascript/1-javascript-basics/script.js
+++ 308-intro-javascript/1-javascript-basics/script.js
@@ -1,5 +1,39 @@
 console.log("Hello World!")
 
+var myVariable = "Some value";
+let mySecond = true;
+
+// constant
+const PI = 3.14;
+// PI = 3.1415; // error
+console.log('test')
+// declaration
+let its_pie_day_today;
+
+let SSN = "7898897"
+SSN = "391401"
+SSN = true
+SSN = null
+SSN = 123
+
+const something = "123";
+console.log(something)
+const something2 = "456";
+console.log(something2)
+
+
+// declaration + assign / initialization
+// let its_pie_day_today = "awesome!";
+
+// assign / initialization 
+its_pie_day_today = "awesome!";
+
+// reassign 
+its_pie_day_today = "cool!";
+
+
+
+
 // code...
 
 /*
