4fd40a94d58974d2154bae6cd0ec07512ff0556f
jadedrb <jadecdrb@gmail.com>
2024-03-14 15:39:48 -0400

    some more on strings

--- 308-intro-javascript/1-javascript-basics/script.js
+++ 308-intro-javascript/1-javascript-basics/script.js
@@ -1,4 +1,4 @@
-console.log("Hello World!")
+console.log("Hello World!");
 
 var myVariable = "Some value";
 let mySecond = true;
@@ -6,39 +6,37 @@ let mySecond = true;
 // constant
 const PI = 3.14;
 // PI = 3.1415; // error
-console.log('test')
+console.log("test");
 // declaration
 let its_pie_day_today;
 
-let SSN = "7898897"
-SSN = "391401"
-SSN = true
-SSN = null
-SSN = 123
+let SSN = "7898897";
+SSN = "391401";
+SSN = true;
+SSN = null;
+SSN = 123;
 
 const something = "123";
-console.log(something)
+console.log(something);
 const something2 = "456";
-console.log(something2)
-
+console.log(something2);
 
 // declaration + assign / initialization
 // let its_pie_day_today = "awesome!";
 
-// assign / initialization 
+// assign / initialization
 its_pie_day_today = "awesome!";
 
-// reassign 
+// reassign
 its_pie_day_today = "cool!";
-
 
 let myVar = true;
 let myNum = 1;
 let myStr = "test";
 
-console.log(typeof myVar)
-console.log(typeof myNum)
-console.log(typeof myStr)
+console.log(typeof myVar);
+console.log(typeof myNum);
+console.log(typeof myStr);
 
 // code...
 
@@ -54,16 +52,15 @@ console.log(typeof myStr)
 let counter = 1;
 
 {
-    let counter = 2;
-    // console.log(counter)
+  let counter = 2;
+  // console.log(counter)
 }
-
 
 // console.log(counter);
 
-let number = (3 + 3) * 8 / (10 - 3); // 6
+let number = ((3 + 3) * 8) / (10 - 3); // 6
 
-console.log(number)
+console.log(number);
 
 let newNum = 9;
 let newNum2 = 9;
@@ -78,25 +75,25 @@ let newNum2 = 9;
 // console.log(newNum)
 
 // post-increment
-console.log(newNum++) // 9
-console.log(newNum) // 10
+console.log(newNum++); // 9
+console.log(newNum); // 10
 
 // pre-increment
-console.log(++newNum2) // 10
-console.log(newNum2) // 10
+console.log(++newNum2); // 10
+console.log(newNum2); // 10
 
-console.log(3 > 4) // false
-console.log(5 < 10) // false
-console.log(78 >= 90) // false
-console.log(56 <= 44) // false
+console.log(3 > 4); // false
+console.log(5 < 10); // false
+console.log(78 >= 90); // false
+console.log(56 <= 44); // false
 
 // loose equality
 console.log(10 == 10); // true
 
-// strict equality 
+// strict equality
 console.log(10 === "10"); // false
 
-console.log(10 != 30) // true
+console.log(10 != 30); // true
 
 // assignment operator
 // =      ex:  let num = 10;
@@ -108,14 +105,102 @@ console.log(10 != 30) // true
 // ===     ex:   10 === "10" -> false
 
 // Logical AND where both need to be true
-let thisLogic = 3 > 2 && 6 == 6  // true
+let thisLogic = 3 > 2 && 6 == 6; // true
 
 // Logical OR where one needs to be true
 //                false    true
-let thisLogic2 = 3 > 3 || 6 == 6  // true
+let thisLogic2 = 3 > 3 || 6 == 6; // true
 
-console.log(thisLogic)
+console.log(thisLogic);
 
 // let lightSwitch = true
 
 // console.log(!lightSwitch)
+
+// string concatenation
+
+console.log("Hello" + " " + "World");
+
+let str1 = "Hello";
+let str2 = "World";
+
+console.log(str1 + " " + str2);
+
+let fruit = "Orange";
+
+console.log(fruit[0]); // p
+console.log("Orange"[0]); // p
+
+fruit[0] = "A";
+
+console.log(fruit);
+
+const name = "Inigo Montoya";
+
+// name[2] = "X";
+
+console.log(name[6]); // M
+
+// const frost =
+//   "Nature’s first green is gold, " +
+//   "Her hardest hue to hold. " +
+//   "Her early leaf’s a flower; " +
+//   "But only so an hour. " +
+//   "Then leaf subsides to leaf. " +
+//   "So Eden sank to grief, " +
+//   "So dawn goes down to day. " +
+//   "Nothing gold can stay. ";
+
+// console.log(frost);
+
+
+// const frost =
+// 	"Nature’s first green is gold, \
+// Her hardest hue to hold. \
+// Her early leaf’s a flower; \
+// But only so an hour. \
+// Then leaf subsides to leaf. \
+// So Eden sank to grief, \
+// So dawn goes down to day. \
+// Nothing gold can stay.";
+
+// console.log(frost);
+
+
+// const frost =
+// 	"Nature’s first green is gold, \n\
+// Her hardest hue to hold. \n\
+// Her early leaf’s a flower; \n\
+// But only so an hour. \n\
+// Then leaf subsides to leaf. \n\
+// So Eden sank to grief, \n\
+// So dawn goes down to day. \n\
+// Nothing gold can stay.";
+
+const frost =
+	`Nature’s first green is gold,
+Her hardest hue to hold.
+Her early leaf’s a flower;
+But only so an hour.
+Then leaf subsides to leaf.
+So Eden sank to grief,
+So dawn goes down to day.
+Nothing gold can stay.`;
+
+console.log(frost);
+
+'  "good"  '
+
+"  'good'  "
+
+" \"good\" "
+
+' \'good\' '
+
+// template literal
+let test = ` "good" 'good'  `
+
+let firstName = 'Bob'
+let lastName = 'Bobby'
+
+console.log(`Hi! My name is ${firstName} ${lastName}. I have \$2!`)
