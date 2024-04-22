cb03f048e5e3252ab360a3c056ff0b032e3f8d93
jadedrb <jadecdrb@gmail.com>
2024-04-22 13:40:42 -0400

    post request example

--- 318-node-and-express/3-restful-api/index.js
+++ 318-node-and-express/3-restful-api/index.js
@@ -7,6 +7,8 @@ const port = 3000
 const posts = require('./data/posts')
 const users = require('./data/users')
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello! (from Server)')
 })
@@ -15,23 +17,62 @@ app.get('/api/posts', (req, res) => {
     res.json(posts)
 })
 
-app.get('/api/users', (req, res) => {
-    res.json(users)
-})
+app
+    .route('/api/users')
+    .get((req, res) => {
+        res.json(users)
+    })
+    .post((req, res) => {
+       console.log('req.body: ', req.body)
 
-app.get('/api/posts/:id', (req, res) => {
-    //     const post = posts.find((post) => post.id === Number(req.params.id))
+       if (req.body.name && req.body.username && req.body.email) {
+        
+        const foundUser = users.find((u) => u.username == req.body.username)
+        
+        if (foundUser) {
+          res.json({ error: "Username Already Taken" });
+          return;
+        }
+  
+        const user = {
+          id: users[users.length - 1].id + 1,
+          name: req.body.name,
+          username: req.body.username,
+          email: req.body.email,
+        };
+  
+        users.push(user);
+        res.json(users[users.length - 1]);
+      } else res.json({ error: "Insufficient Data" });
+
+    })
+
+
+
+app.get('/api/posts/:id', (req, res, next) => {
     const post = posts.find((post) => post.id == req.params.id)
-    console.log(post)
-    res.json(post)
+    if (post) {
+        res.json(post)
+    } else {
+        next()
+    }
 })
 
-app.get('/api/users/:id', (req, res) => {
+app.get('/api/users/:id', (req, res, next) => {
     const user = users.find((user) => user.id == req.params.id)
-    console.log(user)
-    res.json(user)
+    if (user) {
+        res.json(user)
+    }
+    else {
+        next()
+    }
 })
 
+// error-handling middleware
+app.use((req, res, next) => {
+    res.status = 404;
+    res.json({ error: 'Resource Not Found' })
+})
 
 
 app.listen(port, () => {
