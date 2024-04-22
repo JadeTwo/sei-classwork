1c404380369a092f5a0f8cfba1732d584d883668
jadedrb <jadecdrb@gmail.com>
2024-04-22 12:43:21 -0400

    added more specific routes with url params

--- 318-node-and-express/3-restful-api/index.js
+++ 318-node-and-express/3-restful-api/index.js
@@ -11,9 +11,28 @@ app.get('/', (req, res) => {
     res.send('Hello! (from Server)')
 })
 
-app.get('/posts', (req, res) => {
+app.get('/api/posts', (req, res) => {
     res.json(posts)
 })
+
+app.get('/api/users', (req, res) => {
+    res.json(users)
+})
+
+app.get('/api/posts/:id', (req, res) => {
+    //     const post = posts.find((post) => post.id === Number(req.params.id))
+    const post = posts.find((post) => post.id == req.params.id)
+    console.log(post)
+    res.json(post)
+})
+
+app.get('/api/users/:id', (req, res) => {
+    const user = users.find((user) => user.id == req.params.id)
+    console.log(user)
+    res.json(user)
+})
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
