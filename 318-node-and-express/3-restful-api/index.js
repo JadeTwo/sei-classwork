caf3a4c5c20ec8c0d21c7252441e943aed19fb92
jadedrb <jadecdrb@gmail.com>
2024-04-22 14:04:34 -0400

    patch and post requests

--- 318-node-and-express/3-restful-api/index.js
+++ 318-node-and-express/3-restful-api/index.js
@@ -13,9 +13,29 @@ app.get('/', (req, res) => {
     res.send('Hello! (from Server)')
 })
 
-app.get('/api/posts', (req, res) => {
-    res.json(posts)
-})
+app
+    .route('/api/posts')
+    .get((req, res) => {
+        res.json(posts)
+    })
+    .post((req, res) => {
+
+    // Within the POST request route, we create a new
+    // post with the data given by the client.
+        if (req.body.userId && req.body.title && req.body.content) {
+
+        const post = {
+          id: posts[posts.length - 1].id + 1,
+          userId: req.body.userId,
+          title: req.body.title,
+          content: req.body.content,
+        };
+  
+        posts.push(post);
+        res.json(posts[posts.length - 1]);
+      } else res.json({ error: "Insufficient Data" });
+
+    })
 
 app
     .route('/api/users')
@@ -49,24 +69,54 @@ app
 
 
 
-app.get('/api/posts/:id', (req, res, next) => {
-    const post = posts.find((post) => post.id == req.params.id)
-    if (post) {
-        res.json(post)
-    } else {
-        next()
-    }
-})
+app
+    .route('/api/posts/:id')
+    .get((req, res, next) => {
+        const post = posts.find((post) => post.id == req.params.id)
+        if (post) {
+            res.json(post)
+        } else {
+            next()
+        }
+    })
+    .patch((req, res) => {
 
-app.get('/api/users/:id', (req, res, next) => {
-    const user = users.find((user) => user.id == req.params.id)
-    if (user) {
-        res.json(user)
-    }
-    else {
-        next()
-    }
-})
+        // Within the PATCH request route, we allow the client
+        // to make changes to an existing post in the database.
+        const post = posts.find((p, i) => {
+            if (p.id == req.params.id) {
+                for (const key in req.body) {
+                    posts[i][key] = req.body[key];
+                }
+                return true;
+            }
+        });
+    
+        if (post) res.json(post);
+        else next();
+
+    })
+    .delete((req, res) => {
+
+    })
+
+app
+    .route('/api/users/:id')
+    .get((req, res, next) => {
+        const user = users.find((user) => user.id == req.params.id)
+        if (user) {
+            res.json(user)
+        }
+        else {
+            next()
+        }
+    })
+    .patch((req, res) => {
+
+    })
+    .delete((req, res) => {
+
+    })
 
 // error-handling middleware
 app.use((req, res, next) => {
