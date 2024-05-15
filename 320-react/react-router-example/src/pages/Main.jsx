733392602d6ad1a033bd7438707659f42b9a16d6
jadedrb <jadecdrb@gmail.com>
2024-05-15 13:48:50 -0400

    example with fetch

--- 320-react/react-router-example/src/pages/Main.jsx
+++ 320-react/react-router-example/src/pages/Main.jsx
@@ -1,4 +1,5 @@
 function Main() {
+
     return (
         <div>Main Page</div>
     )
--- 320-react/react-router-example/src/pages/Profile.jsx
+++ 320-react/react-router-example/src/pages/Profile.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 function Profile() {
 
-    const { id } = useParams() // return ->   { id: 'bob }
+    const [user, setUser] = useState(null)
+
+    const { id } = useParams() 
 
     console.log(id)
 
+    useEffect(() => {
+        async function getUserData() {
+            try {
+                let response = await fetch('http://example-api.com/' + id)
+                let userData = await response.json()
+                setUser(userData)
+            } catch(err) {
+                console.log('whoops...')
+                console.log(err)
+                setUser('No user available...')
+            }
+        }
+        getUserData()
+    }, [])
+
     return (
-        <div>Profile Page</div>
+        <div>
+            Profile Page
+            <br />
+            {user}
+        </div>
     )
 }
 
