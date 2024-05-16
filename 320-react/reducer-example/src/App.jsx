ce27b205738fdfb6d780fe6acb0cd3ddcda5a7d9
jadedrb <jadecdrb@gmail.com>
2024-05-16 12:42:25 -0400

    added some more cases

--- 320-react/reducer-example/src/App.jsx
+++ 320-react/reducer-example/src/App.jsx
@@ -1,10 +1,11 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 import './App.css'
 
 // TYPES
 const INCREMENT = 'increment'
 const DECREMENT = 'decrement'
 const RESET = 'reset'
+const SET_NUMBER = 'set_number'
 
 function App() {
 
@@ -12,13 +13,16 @@ function App() {
   // action is the information from dispatch
   function reducer(state, action) {
     // ...
+    // action -> { type: ... , payload: ... }
     switch(action.type) {
       case INCREMENT:
-        return state + 1
+        return state + action.payload
       case 'decrement':
-        return state - 1
+        return state - action.payload
       case RESET:
         return 0
+      case SET_NUMBER:
+        return action.payload
       default: 
         return state
     }
@@ -38,6 +42,7 @@ function App() {
   // dispatch -> reducer gets info from dispatch -> and returns a new state
 
   const [count, dispatch] = useReducer(reducer, 0) // return ->  [stateVariable, function]
+  const [num, setNum] = useState(1)
 
   return (
     <>
@@ -45,11 +50,15 @@ function App() {
       <br />
       <h1>Counter: {count}</h1>
       <br />
-      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'increment', payload: num })}>Increment</button>
       <br />
-      <button onClick={() => dispatch({ type: DECREMENT })}>Decrement</button>
+      <button onClick={() => dispatch({ type: DECREMENT, payload: num })}>Decrement</button>
+      <br />
+      <button onClick={() => dispatch({ type: SET_NUMBER, payload: num })}>Set Number</button>
       <br />
       <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <br />
+      <input type="number" value={num} onChange={(e) => setNum(Number(e.target.value))} />
     </>
   )
 }
